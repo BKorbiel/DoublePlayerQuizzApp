4efff21786a8da81056e43f9ec4d6071f864a987
bkorbiel <korbiel.bartek@gmail.com>
2023-09-17 14:00:05 +0200

    Created Auth component

---
+++ app/screens/Auth/Auth.tsx
@@ -0,0 +1,48 @@
+import { View, Text, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { FIREBASE_AUTH } from "../../../firebaseConfig";
+import styles from "./styles";
+
+const Auth = () => {
+    const auth = FIREBASE_AUTH;
+    const [isSignUp, setIsSingUp] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+    const [username, setUsername] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <Text>{isSignUp ? "Create a new account" : "Sign In"}</Text>
+            {isSignUp && 
+                <TextInput
+                    value={email}
+                    placeholder="Enter your email"
+                    onChangeText={(text) => setEmail(text)}
+                />
+            }
+            <TextInput
+                value={username}
+                placeholder="Enter your username"
+                onChangeText={(text) => setUsername(text)}
+                />
+            <TextInput
+                value={password}
+                placeholder="Enter your password"
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry={true}
+                />
+            {isSignUp && 
+                <TextInput
+                    value={passwordConfirm}
+                    placeholder="Confirm password"
+                    onChangeText={(text) => setPasswordConfirm(text)}
+                />
+            }
+            <Button title={"Sign In"} onPress={() => {}}/>
+            <Button title={"Don't have an account yet? Create a new one"} onPress={() => setIsSingUp(true)}/>
+        </View>
+    )
+}
+
+export default Auth;
